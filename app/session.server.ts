66e11adbb5068b941da03f73f4eabdc8f979e859
Luke Digby <luke.digby1@gmail.com>
2022-04-25 12:26:53 +0100

    Remove unnecessary packages

--- app/session.server.ts
+++ app/session.server.ts
@@ -6,17 +6,8 @@ import { getUserById } from "~/models/user.server";
 
 invariant(process.env.SESSION_SECRET, "SESSION_SECRET must be set");
 
-export const sessionStorage = createCookieSessionStorage({
-  cookie: {
-    name: "__session",
-    httpOnly: true,
-    maxAge: 0,
-    path: "/",
-    sameSite: "lax",
-    secrets: [process.env.SESSION_SECRET],
-    secure: process.env.NODE_ENV === "production",
-  },
-});
+// TODO - use Cognito session storage
+export const sessionStorage: any;
 
 const USER_SESSION_KEY = "userId";
 
